262e98275e9bc72b4f69a83ec123316454ccca09
Walker <walker@praiseweb.com.br>
2018-01-21 13:47:09 -0200

    add debug

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ var scrape = require('website-scraper');
 var argv = require('yargs').argv;
 
 var options = {
-    urls: typeof argv.url === 'array'?argv.url:[argv.url],
+    urls: typeof argv._ === 'array'?argv._:[argv._],
     directory: argv.dist||'dist',
     recursive: argv.recursive||true,
     maxRecursiveDepth: argv.depth||1
 };
-
 // with promise
 scrape(options).then(console.log).catch(console.error);
